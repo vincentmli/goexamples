24335b928842bbe36cc67a0efe060aa10f3cbc3e
Vincent Li <v.li@f5.com>
2021-04-29 17:42:29 -0700

    Add static build OS and go version

--- tcp-listener.go
+++ tcp-listener.go
@@ -1,69 +1,73 @@
+/*
+ version go1.15.5 linux/amd64 on Ubuntu 20.04
+ compile static build
+ CGO_ENABLED=0 GOOS=linux go build -a  -o tcp-listener tcp-listener.go
+*/
+
 package main
 
 import (
-    "flag"
-    "os"
-    "os/exec"
-    "os/signal"
-    "syscall"
-    "log"
-    "fmt"
-    "strings"
-    "strconv"
-    "net"
-    "net/http"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
 )
 
 var mask string = "24"
 
 func Hosts(cidr string) ([]string, int, error) {
-    ip, ipnet, err := net.ParseCIDR(cidr)
-    if err != nil {
-        return nil, 0, err
-    }
+	ip, ipnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, 0, err
+	}
 
-    var ips []string
-    for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
-        ips = append(ips, ip.String())
-    }
+	var ips []string
+	for ip := ip.Mask(ipnet.Mask); ipnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
 
-    // remove network address and broadcast address
-    lenIPs := len(ips)
-    switch {
-    case lenIPs < 2:
-        return ips, lenIPs, nil
+	// remove network address and broadcast address
+	lenIPs := len(ips)
+	switch {
+	case lenIPs < 2:
+		return ips, lenIPs, nil
 
-    default:
-    return ips[1 : len(ips)-1], lenIPs - 2, nil
-    }
+	default:
+		return ips[1 : len(ips)-1], lenIPs - 2, nil
+	}
 }
 
 func inc(ip net.IP) {
-    for j := len(ip) - 1; j >= 0; j-- {
-        ip[j]++
-        if ip[j] > 0 {
-            break
-        }
-    }
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
 }
-
-
 
 func hello(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "hello world")
+	fmt.Fprintf(w, "hello world")
 }
 
-func execCmd(command string, argsv []string) (err error){
-    args := argsv
-    cmdObj := exec.Command(command, args...)
-    cmdObj.Stdout = os.Stdout
-    cmdObj.Stderr = os.Stderr
-    err = cmdObj.Run()
-    if err != nil {
-        log.Fatal(err)
-        return err
-    }
-    return nil
+func execCmd(command string, argsv []string) (err error) {
+	args := argsv
+	cmdObj := exec.Command(command, args...)
+	cmdObj.Stdout = os.Stdout
+	cmdObj.Stderr = os.Stderr
+	err = cmdObj.Run()
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
 }
 
 func SetupCloseHandler(cidr string, device string) {
@@ -80,7 +84,7 @@ func SetupCloseHandler(cidr string, device string) {
 func RemoveIps(cidr string, device string) {
 	fmt.Println("- Run Clean Up - Remove IPs")
 
-        hosts, _, err := Hosts(cidr)
+	hosts, _, err := Hosts(cidr)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -99,7 +103,7 @@ var Usage = func() {
 }
 
 func blockForever() {
-    select{ }
+	select {}
 }
 
 func main() {
@@ -113,14 +117,14 @@ func main() {
 
 	start, err := strconv.Atoi(ports[0])
 	if err != nil {
-        // handle error
+		// handle error
 		fmt.Println(err)
 		os.Exit(2)
 	}
 
 	end, err := strconv.Atoi(ports[1])
 	if err != nil {
-        // handle error
+		// handle error
 		fmt.Println(err)
 		os.Exit(2)
 	}
@@ -130,11 +134,10 @@ func main() {
 	serverMux := http.NewServeMux()
 	serverMux.HandleFunc("/", hello)
 
-        hosts, _, err := Hosts(*cidr)
+	hosts, _, err := Hosts(*cidr)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 
 	for _, h := range hosts {
 		ipaddr := fmt.Sprintf("%s/%s", h, mask)
